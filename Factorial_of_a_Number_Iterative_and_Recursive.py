27ecc94f55fbfa541e74591f3d4c2f4b77641531
Vivek <sonkar.vivek@yahoo.com>
2024-09-02 22:16:49 +0530

    added in folder

--- Factorial_of_a_Number_Iterative_and_Recursive.py
+++
@@ -1,6 +0,0 @@
-def Factorial_of_a_Number_Iterative_and_Recursive(n):
-  if (n <= 0):
-    return 1
-  else:
-    return n * Factorial_of_a_Number_Iterative_and_Recursive(n-1)
-print(Factorial_of_a_Number_Iterative_and_Recursive(10))
